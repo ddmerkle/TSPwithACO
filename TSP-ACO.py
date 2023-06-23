8cd880d55b43d9b2f189a7d001bc2febeca6c607
tlevitt1 <tom_levitt@ymail.com>
2023-06-22 20:51:38 -0400

    starting

---
+++ TSP-ACO.py
@@ -0,0 +1,16 @@
+# Authors: Dawson Merkle : Thomas Levitt
+# Date: 6/22/23
+# Assignment 3
+# CAP 4630
+
+
+
+
+
+def main():
+    '''main function'''
+    print('aksjdfhjkas')
+
+
+if __name__ == '__main__':
+    main()
