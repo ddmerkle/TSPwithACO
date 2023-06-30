a24763ab34047bf15f4039fb7973f6e44b8dc02e
tlevitt1 <tom_levitt@ymail.com>
2023-06-30 00:58:23 -0400

    Made create_probabilities and removed the dictionary

--- TSP-ACO.py
+++ TSP-ACO.py
@@ -62,14 +62,15 @@ class City():
         return self.x, self.y
     
     def get_name(self):
-        '''returns name'''
+        '''returns string representation as function.
+            Usable as object call'''
 
-        return "({name})".format(name = self.name)
+        return '({name})'.format(name = self.name)
     
     def __repr__(self):
         '''returns string representation of city'''
 
-        return "({name}: x:{x}, y:{y})".format(name = self.name, 
+        return '({name}: x:{x}, y:{y})'.format(name = self.name, 
                                          x = self.x, y = self.y)
     
 
@@ -80,26 +81,26 @@ class Ant():
         '''constructor'''
     
         self.name = name
-        self.tourLis = []
+        self.tourList = []
         
         #defines starting point in list
-        self.tourLis.append(startPoint)
+        self.tourList.append(startPoint)
     
     def visit(self, cityObject):
         '''Ant has visited this city'''
-        self.tourLis.append(cityObject)
+        self.tourList.append(cityObject)
 
     def get_tour(self):
 
-        return self.tourLis
+        return self.tourList
     
     # def get_distance_traveled(self):
     
     def __repr__(self):
         '''returns string representation of ant'''
     
-        return '({name}: list: {tourLis})'.format(name = self.name, 
-                                                   tourLis = self.tourLis)
+        return '({name}: list: {tourList})'.format(name = self.name, 
+                                                   tourList = self.tourList)
 
 
 #################################################
@@ -108,20 +109,62 @@ class Ant():
 #     for city in cityList:
 #         finishtour(city)
 
-
-
-# def do_tour():
-#     probLis = createprobabilities(cityList)
-#     city = roulettewheelselection(probLis)
-#     ant.visit(city)
-#     cityList.delete(city)
-
-
-
 # def visit_city(self, pheromoneTrails):
 # def visit_random_city(self):
 # def visit_probabilistic_city(self, pheromonTrails):
 # def roulette_wheel_selection(self, probabilities):
+
+
+
+
+
+def do_tour(ant, cityList, strCityList, disMatrix, pheratrix, ALPHA, BETA):
+    '''does tour for all ants'''
+    probList = create_probabilities(ant, cityList, strCityList, disMatrix, 
+                                    pheratrix, ALPHA, BETA)
+
+def create_probabilities(ant, cityList, strCityList, disMatrix, pheratrix, 
+                         ALPHA, BETA):
+    '''create probabilities for cities to be used in roulette for an ant'''
+
+    print(str(ant))
+    print('\n')
+
+    denominator = 0
+    currentCity = cityList.index(ant.get_tour()[-1])
+    unVisitedList = []
+    probList = []
+    # print(currentCity)
+
+    for city in cityList:
+        if city not in ant.get_tour():
+            i = strCityList.index(city.__repr__())
+            unVisitedList.append(i)
+            #print(unVisitedList)
+
+    for city in unVisitedList:
+        # print(city)
+        if currentCity == city:
+            continue
+
+        denominator += ((pheratrix[currentCity][city]**ALPHA) * 
+                        ((1/disMatrix[currentCity][city])**BETA))
+
+    for city in unVisitedList:
+        if currentCity == city:
+            continue
+
+        numarator = ((pheratrix[currentCity][city]**ALPHA) * 
+                        ((1/disMatrix[currentCity][city])**BETA))
+
+        cityProb = numarator / denominator
+
+        probList.append(cityProb)
+
+    print('These are the probabilities')
+    print(probList)
+    print('\n')
+    return probList
 
 
 def proportional_roulette_selection(population, normProbabilities):
@@ -168,21 +211,15 @@ def create_distance_matrix(cityList):
         for y in range(len(cityList)):
             disMatrix[x][y] = cityList[x].get_distance(cityList[y])
 
-
-    strCityList = []
-    for city in cityList:
-        strCityList.append(str(city.__repr__()))
-
-    disMatDict = dict(zip(strCityList, disMatrix))
-    print('!!!!!!!!!!!!!!')
+    # disMatDict = dict(zip(strCityList, disMatrix))
+    # print('!!!!!!!!!!!!!!')
     
-    i = list(disMatDict).index('(Palm Bay: x:176, y:155)')
-    j = list(disMatDict).index('(Key West: x:179, y:194)')
-    print(i)
-    print(j)
-    print(disMatrix[i][j])
-    print('!!!!!!!!!!!!!!')
-
+    # i = list(disMatDict).index('(Palm Bay: x:176, y:155)')
+    # j = list(disMatDict).index('(Key West: x:179, y:194)')
+    # print(i)
+    # print(j)
+    # print(disMatrix[i][j])
+    # print('!!!!!!!!!!!!!!')
 
     '''
     NEED
@@ -205,23 +242,10 @@ def create_distance_matrix(cityList):
     at index 0 of disMatrix so disMatrix[0] is Boca and disMatrix[2] 
     is key west. disMatrix[0][2] will give us the distance between those two 
     cities'''
-
-        
-    # print(disMatDict.get(strCityList[index]))
-    print('!!!!!!!!!!!!!!')
-
-
-    # for city in cityList:
-    #     for route in disMatrix:
-    #         disMatDict[city.get_name()] = route
-    #         break
-
-    # print('\n')
-    # print(disMatrix[0][9])
-    # print('\n')
-    # print(disMatrix[9])
     
-    return disMatrix, disMatDict
+    # return disMatrix, disMatDict
+
+    return disMatrix
 
 
 def create_cities(NUM_ANT_FACTOR, NUM_CITY):
@@ -253,7 +277,11 @@ def create_cities(NUM_ANT_FACTOR, NUM_CITY):
                             x=int(random.random() * 200), 
                              y=int(random.random() * 200)))
         
-    return cityList
+    strCityList = []
+    for city in cityList:
+        strCityList.append(str(city.__repr__()))
+        
+    return cityList, strCityList
         
 
 
@@ -265,7 +293,7 @@ def main():
     NUM_ANT_FACTOR = 1
     NUM_CITY = 10
     ALPHA = 1
-    BETA  = 1
+    BETA  = 2
     RANDOM_ATTRACTION_FACTOR = 0.5
     NUM_ITERATIONS = 10
     EVAPORATION_RATE = 0.5
@@ -273,42 +301,64 @@ def main():
 
     random.seed(1)
 
-    cityList = create_cities(NUM_ANT_FACTOR, NUM_CITY)
-    # print('\n')
-    # print('This is the city list: ')
-    # print(cityList)
-    # print('\n')
+    cityList, strCityList = create_cities(NUM_ANT_FACTOR, NUM_CITY)
+    print('\n')
+    print('This is the city list in object format: ')
+    print(cityList)
+    print('!!!!!!!!!!!!!!')
+    print('This is the city list in str format: ')
+    print(strCityList)
+    print('\n')
 
+    disMatrix = create_distance_matrix(cityList)
+    print('\n')
+    for row in disMatrix:
+        print(row)
+
+    i = strCityList.index('(Palm Bay: x:176, y:155)')
+    j = strCityList.index('(Key West: x:179, y:194)')
+
+    ant = disMatrix[i][j]
+    print('!!!!!!!!!!!!!!')
+    print(ant)
+    print('!!!!!!!!!!!!!!')
 
     pheratrix = create_pheromone_matrix(cityList)
     print('\n')
     for row in pheratrix:
         print(row)
-        
-    
 
-    # create_distance_matrix(cityList)
-    disMatrix, disMatDict = create_distance_matrix(cityList)
+    antColony = create_colony(cityList, NUM_ANT_FACTOR, NUM_CITY)
     print('\n')
-    for row in disMatrix:
-        print('\n')
-        print(row)
+    print('This is the colony: ')
+    print(antColony)
+    print('\n')
+
+    for ant in antColony:
+        do_tour(ant, cityList, strCityList, disMatrix, pheratrix, ALPHA, BETA)
+        break
+
+    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    print('done')
 
 
-    print('\n')
-    print(disMatDict)
-    print('\n')
+
+
+    # disMatrix, disMatDict = create_distance_matrix(cityList)
+    # print('\n')
+    # for row in disMatrix:
+    #     print('\n')
+    #     print(row)
+
+    # print('\n')
+    # print(disMatDict)
+    # print('\n')
 
     # print(disMatrix[3][4])
 
     # print(len(disMatrix))
 
-    # antColony = create_colony(cityList, NUM_ANT_FACTOR, NUM_CITY)
-    # print('\n')
-    # print('This is the colony: ')
-    # print(antColony)
-    # print('\n')
-
+    
 
 if __name__ == '__main__':
     main()
